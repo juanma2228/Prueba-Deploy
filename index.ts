59789fdc6f1fb228292891db8f0f086c55d356f9
juanma2228 <juanmacolaneri@outlook.com>
2022-08-11 19:08:24 -0300

    8 prueba

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ const { PORT } = process.env
 const setOffersAndProposals =require('./data'); */
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(PORT, async () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   /*   await setData();
